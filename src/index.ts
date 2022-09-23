93d601561ae73036283b711e039800005160931e
tylim <limteikyee@gmail.com>
2022-09-23 18:52:56 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-export const a = 1
+import { z } from 'zod'
+
+export const a = z.union([z.object({ a: z.number() }), z.boolean()])
+export const b = z.object({ a: z.object({ a: 1 }) })
+console.log(a._def)
